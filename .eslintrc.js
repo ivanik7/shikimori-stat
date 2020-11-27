138674e636e51437769e9eaa9befd8b0e244f391
ivanik <ivan170102@gmail.com>
2020-11-27 13:48:59 +0500

    Обнавление зависимостей

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: [
+    "airbnb-base",
+    "plugin:prettier/recommended",
+    "plugin:jest/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "no-restricted-syntax": [
+      "error",
+      "WithStatement",
+      "BinaryExpression[operator='in']"
+    ],
+    "no-await-in-loop": "off",
+    "no-continue": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { vars: "all", args: "after-used", ignoreRestSiblings: false }
+    ],
+    "lines-between-class-members": "off",
+    "no-restricted-globals": "off",
+    "import/extensions": "off"
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".ts"]
+      }
+    }
+  }
+};
